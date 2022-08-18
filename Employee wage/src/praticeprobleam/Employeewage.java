c8f2e7358b880be9d9a9067bd2b6adb85a20a37b
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-18 13:51:04 +0530

    welcome to employeewage computation

---
+++ Employee wage/src/praticeprobleam/Employeewage.java
@@ -0,0 +1,10 @@
+package praticeprobleam;
+
+public class Employeewage {
+	public static void main(String[] args) {
+		System.out.println  (" welcome to employee wage computation program" );
+
+	
+}
+
+}
