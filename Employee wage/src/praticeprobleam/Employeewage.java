45bba567f8df56ac9b9fa1fe3b46b89614cc2123
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-18 18:11:52 +0530

    Check Employee is Present or Absent Use((RANDOM) for Attendance Check

--- Employee wage/src/praticeprobleam/Employeewage.java
+++ Employee wage/src/praticeprobleam/Employeewage.java
@@ -3,8 +3,15 @@ package praticeprobleam;
 public class Employeewage {
 	public static void main(String[] args) {
 		System.out.println  (" welcome to employee wage computation program" );
+		double empcheck = Math.floor(Math.random()*10%2);
+		if (empcheck == 1) {
+			System.out.println("emplyee is present");
+		}else {
+			System.out.println("emplyee is absent");
+
 
 	
 }
 
+}
 }
