d51fc7bbfc2d1a66fcb0798e213a910167efff05
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-18 18:33:30 +0530

    Add Part time Employee & Wage- Assume Part time Hour is 4

--- Employee wage/src/praticeprobleam/Employeewage.java
+++ Employee wage/src/praticeprobleam/Employeewage.java
@@ -3,24 +3,25 @@ package praticeprobleam;
 public class Employeewage {
 	public static void main(String[] args) {
 		System.out.println  (" welcome to employee wage computation program" );
-		int IS_FULL_TIME = 1; 
-		int EMP_RATE_PER_HOUR = 20;
-
-		int empHrs = 0;
-		int empwage= 0;
-
-		double empCheck = Math.floor(Math.random() * 10) %2; 
-		if (empCheck == IS_FULL_TIME)
-
-		empHrs = 8;
-
+		int IS_PART_TIME =1;
+		int IS_FULL_TIME =2;
+		int EMP_RATE_PER_HOUR= 20;
+		 
+		int empHRs =0;
+		int empwage=0;
+	    double empcheck = Math.floor(Math.random() *10 % 3 );
+		if ( empcheck == IS_PART_TIME) {
+		    empHRs = 4 ;
+		}
+		else if (empcheck == IS_FULL_TIME)
+		empHRs=8;
 		else 
-			empHrs = 0;
-
-		empwage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empwage);
-
+		empHRs=0;
+		empwage = empHRs * EMP_RATE_PER_HOUR;
+	    System.out.println("Emy wage : " + empwage );
 	
 }
+	}
 
-}
+
+
